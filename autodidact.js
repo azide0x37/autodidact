80e814a5885c40c7b071697589fa66422a87c491
Alexander Templeton <blakmatr@gmail.com>
2015-04-27 22:56:24 +0000

    JSON parsing and display sorted

--- autodidact.js
+++ autodidact.js
@@ -1,23 +1,35 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    Template.meh.helpers({
+      'photo': function(){
+           Meteor.call('getMeh',function(err,results){
+           console.log(results.content);
+           Session.set('photo', JSON.parse(results.content).deal.photos[0]);
+       });
+    return (Session.get('photo'));
+   },
+      'features': function(){
+        Meteor.call('getMeh', function(err, results){
+          console.log(results.content);
+            Session.set('features', JSON.parse(results.content).deal.features);
+          });
+      return (Session.get('features'));
+      }
+});
+    
+  Template.meh.events({
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
   });
+  
+  Meteor.methods({
+      'getMeh':function(){
+          return Meteor.http.call('GET','https://api.meh.com/1/current.json?apikey=bawDoKqvnBeJ9oBhGr7qnlFSwygB9L1D');
+      }
+    })
 }
